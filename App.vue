bd0eb5ca5e798fed81a41d1a17396c473f2dc737
evelina-kopic <92860695+evelina-kopic@users.noreply.github.com>
2022-10-23 22:35:29 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app class="app-main">
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  })
+};
+</script>
+<style src="./assets/tailwind.css">
+.v-application{
+  
+}
+</style>
